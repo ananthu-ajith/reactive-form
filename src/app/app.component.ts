d3132deae01e95a90c950539ae38848739262f44
Ananthu <ananthuajith6587@gmail.com>
2024-04-05 16:59:19 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'reactive';
+  form !:FormGroup;
+  booklist:any[]=[];
+
+  constructor(private fb:FormBuilder)
+  {}
+    ngOnInit()
+    {
+    this.form=this.fb.group({
+      Book_Name:[''],
+      Author:[''],
+      Price:[''],
+      Genre:[''],
+      Published_Date:[''],
+      Country:[''],
+      Description:['']
+    })
+
+    
+    
+    }
+
+    submit()
+    {
+      this.booklist.push(this.form.value);     
+      console.log(this.booklist);
+      this.form.reset();
+    }
+  
 }
